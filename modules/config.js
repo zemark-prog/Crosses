8373b04c006d0c961cc387b20ddd1e812404bf56
zemark-prog <zemark2012@gmail.com>
2020-06-01 18:43:09 +0300

    heroku fixed

--- modules/config.js
+++ modules/config.js
@@ -3,7 +3,7 @@
 require('dotenv').config();
 const MAX_BUTTONS = 8;
 const MIN_BUTTONS = 4;
-const TOKEN = process.env.KEY;
+const TOKEN = process.env.BOT_TOKEN;
 const BOT_URL = process.env.BOT_URL;
 
 module.exports = {
